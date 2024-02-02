76f0c4923be0d2144821cc5d40e5ed475de8c8f3
judigot <judigot@github.com>
2024-02-02 10:33:33 +0800

    feat: add confirmation for discarding unsaved snippet

--- src/components/snippet/AddSnippetForm.tsx
+++ src/components/snippet/AddSnippetForm.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 interface Props {
   closeFormCallback: (value: boolean) => void;
@@ -46,14 +46,75 @@ export default function Form({
     e.preventDefault();
   };
 
+  const handleCloseSnippet = () => {
+    if (formData.snippetInput !== undefined) {
+      // eslint-disable-next-line no-alert
+      if (confirm('Discard unsaved snippet?')) {
+        setIsAddSnippetVisible(false);
+        setFormData(() => ({
+          snippetInput: undefined,
+          snippetTypeInput: undefined,
+          prefixInput: undefined,
+        }));
+      }
+      return;
+    }
+    setIsAddSnippetVisible(false);
+    setFormData(() => ({
+      snippetInput: undefined,
+      snippetTypeInput: undefined,
+      prefixInput: undefined,
+    }));
+  };
+
   const [formData, setFormData] = useState<FormData>({
     snippetInput: undefined,
     snippetTypeInput: undefined,
     prefixInput: undefined,
   });
 
+  useEffect(() => {
+    const snippetInput = document.querySelector(
+      `#snippetInput`,
+    ) as HTMLTextAreaElement;
+    snippetInput.focus();
+  }, []);
+
   return (
     <form method="POST" onSubmit={handleSubmit}>
+      <label htmlFor="snippetInput">
+        Snippet
+        <br />
+        <textarea
+          id="snippetInput"
+          name="snippetInput"
+          value={formData.snippetInput}
+          onChange={handleChange}
+          aria-label="Textarea Input"
+          spellCheck={false}
+          onKeyDown={(e) => {
+            const CTRL_ENTER: boolean =
+              (e.metaKey || e.ctrlKey) && e.key === 'Enter';
+
+            const ESCAPE: boolean = e.key === 'Escape';
+
+            if (ESCAPE) {
+              handleCloseSnippet();
+            }
+
+            if (CTRL_ENTER) {
+              /* prettier-ignore */ (() => { const QuickLog = JSON.stringify(formData, null, 4); const parentDiv = document.getElementById('quicklogContainer') || (() => {const div = document.createElement('div');div.id = 'quicklogContainer';div.style.cssText = 'position: fixed; top: 10px; right: 10px; z-index: 1000;';document.body.appendChild(div);return div; })(); const createChildDiv = (text: typeof QuickLog) => {const newDiv = Object.assign(document.createElement('div'), { textContent: text, style: 'font: bold 25px "Comic Sans MS"; width: max-content; max-width: 500px; word-wrap: break-word; background-color: rgb(255, 240, 0); box-shadow: white 0px 0px 5px 1px; padding: 5px; border: 3px solid black; border-radius: 10px; color: black !important; cursor: pointer;',});const handleMouseDown = (e: MouseEvent) => { e.preventDefault(); const clickedDiv = e.target instanceof Element && e.target.closest('div');if (clickedDiv !== null && e.button === 0 && clickedDiv === newDiv) { const textArea = document.createElement('textarea'); textArea.value = clickedDiv.textContent ?? ''; document.body.appendChild(textArea); textArea.select(); document.execCommand('copy'); document.body.removeChild(textArea);clickedDiv.style.backgroundColor = 'green'; setTimeout(() => { clickedDiv.style.backgroundColor = 'rgb(255, 240, 0)'; }, 1000); }};const handleRightClick = (e: MouseEvent) => { e.preventDefault(); if (parentDiv.contains(newDiv)) { parentDiv.removeChild(newDiv); }};newDiv.addEventListener('mousedown', handleMouseDown);newDiv.addEventListener('contextmenu', handleRightClick);return newDiv; };parentDiv.prepend(createChildDiv(QuickLog)); })()
+            }
+          }}
+          style={{
+            height: '100px',
+            width: '500px',
+            resize: 'none',
+          }}
+        />
+      </label>
+      <br />
+      <br />
       <label htmlFor="prefixInput">
         Prefix
         <br />
@@ -94,48 +155,18 @@ export default function Form({
       </label>
       <br />
       <br />
-      <label htmlFor="snippetInput">
-        Snippet
-        <br />
-        <textarea
-          id="snippetInput"
-          name="snippetInput"
-          value={formData.snippetInput}
-          onChange={handleChange}
-          aria-label="Textarea Input"
-          spellCheck={false}
-          onKeyDown={(e) => {
-            const CTRL_ENTER: boolean =
-              (e.metaKey || e.ctrlKey) && e.key === 'Enter';
-
-            if (CTRL_ENTER) {
-              /* prettier-ignore */ (() => { const QuickLog = JSON.stringify(formData, null, 4); const parentDiv = document.getElementById('quicklogContainer') || (() => {const div = document.createElement('div');div.id = 'quicklogContainer';div.style.cssText = 'position: fixed; top: 10px; right: 10px; z-index: 1000;';document.body.appendChild(div);return div; })(); const createChildDiv = (text: typeof QuickLog) => {const newDiv = Object.assign(document.createElement('div'), { textContent: text, style: 'font: bold 25px "Comic Sans MS"; width: max-content; max-width: 500px; word-wrap: break-word; background-color: rgb(255, 240, 0); box-shadow: white 0px 0px 5px 1px; padding: 5px; border: 3px solid black; border-radius: 10px; color: black !important; cursor: pointer;',});const handleMouseDown = (e: MouseEvent) => { e.preventDefault(); const clickedDiv = e.target instanceof Element && e.target.closest('div');if (clickedDiv !== null && e.button === 0 && clickedDiv === newDiv) { const textArea = document.createElement('textarea'); textArea.value = clickedDiv.textContent ?? ''; document.body.appendChild(textArea); textArea.select(); document.execCommand('copy'); document.body.removeChild(textArea);clickedDiv.style.backgroundColor = 'green'; setTimeout(() => { clickedDiv.style.backgroundColor = 'rgb(255, 240, 0)'; }, 1000); }};const handleRightClick = (e: MouseEvent) => { e.preventDefault(); if (parentDiv.contains(newDiv)) { parentDiv.removeChild(newDiv); }};newDiv.addEventListener('mousedown', handleMouseDown);newDiv.addEventListener('contextmenu', handleRightClick);return newDiv; };parentDiv.prepend(createChildDiv(QuickLog)); })()
-            }
-          }}
-          style={{
-            height: '100px',
-            width: '500px',
-            resize: 'none',
-          }}
-        />
-      </label>
-      <br />
       <br />
       <button type="submit">Submit</button>
       &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
       <button
         type="button"
         onClick={() => {
-          setIsAddSnippetVisible(false);
-          setFormData(() => ({
-            snippetInput: undefined,
-            snippetTypeInput: undefined,
-            prefixInput: undefined,
-          }));
+          handleCloseSnippet();
         }}
       >
         Cancel
       </button>
+      <hr />
     </form>
   );
 }
