bb6abd1837b7b7dda6957ad2bee303c11a9d8136
judigot <judigot@gmail.com>
2024-04-16 16:29:51 +0800

    feat: render prefix snippet type

--- src/components/prefix/Prefixes.tsx
+++ src/components/prefix/Prefixes.tsx
@@ -7,31 +7,39 @@ interface Props {
 export default function Prefixes({ prefixes }: Props) {
   return (
     <section style={{ textAlign: 'center' }}>
-      {prefixes?.map(({ prefix_id, prefix_description, prefix_names }) => (
-        <div className="pb-10" key={prefix_id}>
-          <h2
-            className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-2xl dark:text-white"
-            style={{ textAlign: 'center' }}
-          >
-            {
-              prefix_names.find((prefix_name) => prefix_name.is_default)
-                ?.prefix_name
-            }
-            &nbsp;
-            {prefix_names.length > 1 && (
-              <>
-                (
-                {prefix_names
-                  .filter((prefix_name) => !prefix_name.is_default)
-                  .map((prefix_name) => prefix_name.prefix_name)
-                  .join(', ')}
-                )
-              </>
-            )}
-          </h2>
-          <p>{prefix_description}</p>
-        </div>
-      ))}
+      {prefixes?.map(
+        ({
+          prefix_id,
+          prefix_description,
+          snippet_type_name,
+          prefix_names,
+        }) => (
+          <div className="pb-10" key={prefix_id}>
+            <h2
+              className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-2xl dark:text-white"
+              style={{ textAlign: 'center' }}
+            >
+              {
+                prefix_names.find((prefix_name) => prefix_name.is_default)
+                  ?.prefix_name
+              }
+              &nbsp;
+              {prefix_names.length > 1 && (
+                <>
+                  (
+                  {prefix_names
+                    .filter((prefix_name) => !prefix_name.is_default)
+                    .map((prefix_name) => prefix_name.prefix_name)
+                    .join(', ')}
+                  )
+                </>
+              )}
+            </h2>
+            {snippet_type_name}
+            <p>{prefix_description}</p>
+          </div>
+        ),
+      )}
     </section>
   );
 }
