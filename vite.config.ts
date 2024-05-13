9a05c60150d689fb4af5ebdb86556bfc0549a674
judigot <judigot@gmail.com>
2024-05-13 16:35:39 +0800

    chore: change frontend port

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { TanStackRouterVite } from '@tanstack/router-vite-plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {port: 3000},
   // <testConfig>
   test: {
     globals: true,
